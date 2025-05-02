36d610d0ac8be1799828e58f29610a6859cdba1d
KaranamK <kaushik.karanam@l-p-a.com>
2025-05-02 11:28:44 +0200

    Adding file path to write errors if the instance is app intance is running on linux

--- Kitchen-Recipe/Program.cs
+++ Kitchen-Recipe/Program.cs
@@ -9,67 +9,76 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Queries;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var firebaseAuthority = builder.Configuration["FirebaseAuth:Authority"];
-var firebaseAudience = builder.Configuration["FirebaseAuth:Audience"];
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    
+    var firebaseAuthority = builder.Configuration["FirebaseAuth:Authority"];
+    var firebaseAudience = builder.Configuration["FirebaseAuth:Audience"];
+    
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.Authority = firebaseAuthority;
+            options.Audience = firebaseAudience;
+            options.RequireHttpsMetadata = true;
+        });
+    
+    builder.Services.AddAuthorization();
+    
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    
+    builder.Services.AddSingleton<PayPalService>();
+    builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+    builder.Services.AddScoped<GetRecipeQueryHandler>();
+    builder.Services.AddScoped<AddRecipeCommandHandler>();
+    builder.Services.AddScoped<GetIngredientQueryHandler>();
+    builder.Services.AddScoped<AddIngredientCommandHandler>();
+    builder.Services.AddScoped<UpdateRecipeCommandHandler>();
+    builder.Services.AddScoped<AddProductsCommandHandler>();
+    builder.Services.AddScoped<GetProductsQueryHandler>();
+    
+    builder.Services.AddDbContext<DataContext>(options =>
     {
-        options.Authority = firebaseAuthority;
-        options.Audience = firebaseAudience;
-        options.RequireHttpsMetadata = true;
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
-
-builder.Services.AddAuthorization();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<PayPalService>();
-builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
-builder.Services.AddScoped<GetRecipeQueryHandler>();
-builder.Services.AddScoped<AddRecipeCommandHandler>();
-builder.Services.AddScoped<GetIngredientQueryHandler>();
-builder.Services.AddScoped<AddIngredientCommandHandler>();
-builder.Services.AddScoped<UpdateRecipeCommandHandler>();
-builder.Services.AddScoped<AddProductsCommandHandler>();
-builder.Services.AddScoped<GetProductsQueryHandler>();
-
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
+    
+    builder.Services.AddCors(options =>
     {
-        policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+        options.AddDefaultPolicy(policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
     });
-});
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-});
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
+    
+    builder.Services.AddControllers().AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
+    
+    var app = builder.Build();
+    
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    app.UseHttpsRedirection();
+    app.MapEndpoints();
+    app.UseCors();
+    
+    app.Run();
+}
+catch (Exception ex)
+{
+    File.WriteAllText("/home/Log.txt", ex.ToString());
+    throw;
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-app.MapEndpoints();
-app.UseCors();
-
-app.Run();
